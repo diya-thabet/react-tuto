f419cd367b1af42828d37a81ffe0012fd7a8f02c
diya-thabet <mohamaddhia@gmail.com>
2025-05-11 21:59:12 +0100

    Changes to be committed: 	modified:   tuto/public/index.html 	modified:   tuto/src/App.js 	new file:   tuto/src/Nihaha.css 	new file:   tuto/src/Nihaha.js  Changes not staged for commit: 	modified:   tuto/src/App.js 	deleted:    tuto/src/Nihaha.css 	deleted:    tuto/src/Nihaha.js

--- tuto/src/App.js
+++ tuto/src/App.js
@@ -1,26 +1,35 @@
-import logo from './logo.svg';
+import Nihaha from './Nihaha.js'
 import './App.css';
 
-function App() {
+
+function dhia(){
+  return 7;
+}
+
+function sayHello(){
+  alert("Hello Dhia")
+}
+
+let a = dhia(); 
+
+const elmStyle = {
+  backgroundColor: "black",
+  color: "white", 
+  width: "200px"
+};
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload and welcome Dhia.
-        </p>
-        <p>hi dhia</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={elmStyle} >dhia the {a}th</h1>
+      <Nihaha/>
+      <button onClick={sayHello} > OK </button>
+    
     </div>
   );
 }
 
-export default App;
+
+
+
