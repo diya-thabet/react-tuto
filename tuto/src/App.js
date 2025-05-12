9507b48be8c1379e68b7321e4e08c5805215b1f3
diya-thabet <mohamaddhia@gmail.com>
2025-05-12 12:35:08 +0100

    yes

--- tuto/src/App.js
+++ tuto/src/App.js
@@ -1,30 +1,12 @@
-import Nihaha from './Nihaha.js'
 import './App.css';
 
 
-function dhia(){
-  return 7;
-}
-
-function sayHello(){
-  alert("Hello Dhia")
-}
-
-let a = dhia(); 
-
-const elmStyle = {
-  backgroundColor: "black",
-  color: "white", 
-  width: "200px"
-};
 
 
 export default function App() {
   return (
     <div className="App">
-      <h1 style={elmStyle} >dhia the {a}th</h1>
-      <Nihaha/>
-      <button onClick={sayHello} > OK </button>
+      <h1>HI Dhia this is react course</h1>
     
     </div>
   );
