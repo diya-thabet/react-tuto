6a933de4f3d48e53ac326a3885035a8e9b47a211
diya-thabet <mohamaddhia@gmail.com>
2025-06-09 19:41:27 +0100

    css

--- Project-form/form/src/formComponent/form.js
+++ Project-form/form/src/formComponent/form.js
@@ -2,8 +2,27 @@ import "./form.css";
 
 export default function Form() {
   return (
-    <>
-      <div className="form"></div>
-    </>
+    <div className="form">
+      <form>
+        <label htmlFor="name">name:</label>
+        <input type="text" id="name" />
+
+        <label htmlFor="number">number:</label>
+        <input type="text" id="number" />
+
+        <label htmlFor="age">age:</label>
+        <input type="text" id="age" />
+
+        <label htmlFor="salary">salary:</label>
+        <select id="salary">
+          <option value="">Select</option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
