ad034167cd70dc70c3e62de6869b4bdd76f0f40f
diya-thabet <mohamaddhia@gmail.com>
2025-06-09 20:52:19 +0100

    hook

--- Project-form/form/src/formComponent/form.js
+++ Project-form/form/src/formComponent/form.js
@@ -1,24 +1,54 @@
 import "./form.css";
+import { useState } from "react";
 
 export default function Form() {
+  const [userForm, setForm] = useState({
+    name: "",
+    number: "",
+    age: "",
+    salary: "",
+  });
+
+  console.log(userForm);
+
   function handleSubmit(e) {
     e.preventDefault();
+  }
+
+  function handleName(e) {
+    setForm({ ...userForm, name: e.target.value });
+    console.log(e.target.value);
+  }
+
+  function handleAge(e) {
+    setForm({ ...userForm, age: e.target.value });
+    console.log(e.target.value);
+  }
+
+  function handleSalary(e) {
+    setForm({ ...userForm, salary: e.target.value });
+    console.log(e.target.value);
+  }
+
+  function handleNumber(e) {
+    setForm({ ...userForm, number: e.target.value });
+    console.log(e.target.value);
   }
 
   return (
     <div className="form">
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">name:</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" onChange={handleName} />
 
         <label htmlFor="number">number:</label>
-        <input type="text" id="number" />
+        <input type="text" id="number" onChange={handleNumber} />
 
         <label htmlFor="age">age:</label>
-        <input type="text" id="age" />
+        <input type="text" id="age" onChange={handleAge} />
 
         <label htmlFor="salary">salary:</label>
-        <select id="salary">
+        <select id="salary" onChange={handleSalary}>
           <option value="">Select</option>
           <option value="low">Low</option>
           <option value="medium">Medium</option>
