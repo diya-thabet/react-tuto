c3ca724463f96bbfe89f1561955ce1f708fcb665
diya-thabet <mohamaddhia@gmail.com>
2025-06-11 14:52:53 +0100

    fixing the popups in a standalone component

--- Project-form/form/src/formComponent/errorComponent/PopUps.js
+++ Project-form/form/src/formComponent/errorComponent/PopUps.js
@@ -10,45 +10,46 @@ export default function PopUps({
   return (
     <>
       <div>
-        {(showModal && userAge && numberLength && (
+        {showModal && (
           <div style={overlay}>
             <div style={modal} className="modal-animate">
-              <h2>Submission Successful</h2>
-              <p>Name: {userForm.name}</p>
-              <p>Number: {userForm.number}</p>
-              <p>Age: {userForm.age}</p>
-              <p>Salary: {userForm.salary}</p>
-              <p>Employee: {userForm.employee ? "Yes" : "No"}</p>
+              {/* submission is ok */}
+              {userAge && numberLength && (
+                <>
+                  <h2>Submission Successful</h2>
+                  <p>Name: {userForm.name}</p>
+                  <p>Number: {userForm.number}</p>
+                  <p>Age: {userForm.age}</p>
+                  <p>Salary: {userForm.salary}</p>
+                  <p>Employee: {userForm.employee ? "Yes" : "No"}</p>
+                </>
+              )}
+              {/* submission with age error */}
+              {!userAge && (
+                <>
+                  <h2>Age Should respect the format</h2>
+                  <p>
+                    <b style={{ color: "red" }}>{" Problem: "}</b>
+                    age: {userForm.age} {userForm.age < 18 ? "< 18" : "> 60"}{" "}
+                  </p>
+                </>
+              )}
+              {/* submission with number length error */}
+              {!numberLength && (
+                <>
+                  <h2>Number Should respect the format</h2>
+                  <p>
+                    <b style={{ color: "red" }}>{" Problem: "}</b>
+                    {userForm.number} {" of length= "}
+                    {userForm.number.length}{" "}
+                    {userForm.number.length < 8 ? "< 8" : "> 8"}{" "}
+                  </p>
+                </>
+              )}
               <button onClick={() => setShowModal(false)}>Close</button>
             </div>
           </div>
-        )) ||
-          (!userAge && showModal && numberLength && (
-            <div style={overlay}>
-              <div style={modal} className="modal-animate">
-                <h2>Age Should respect the format</h2>
-                <p>
-                  <b style={{ color: "red" }}>{" Problem: "}</b>
-                  age: {userForm.age} {userForm.age < 18 ? "< 18" : "> 60"}{" "}
-                </p>
-                <button onClick={() => setShowModal(false)}>Close</button>
-              </div>
-            </div>
-          )) ||
-          (!numberLength && showModal && (
-            <div style={overlay}>
-              <div style={modal} className="modal-animate">
-                <h2>Number Should respect the format</h2>
-                <p>
-                  <b style={{ color: "red" }}>{" Problem: "}</b>
-                  {userForm.number} {" of length= "}
-                  {userForm.number.length}{" "}
-                  {userForm.number.length < 8 ? "< 8" : "> 8"}{" "}
-                </p>
-                <button onClick={() => setShowModal(false)}>Close</button>
-              </div>
-            </div>
-          ))}
+        )}
       </div>
     </>
   );
